432c42e95eb492f712591dd8f1e18de3b8d4057e
maryredlinger <mary@redlinger.net>
2020-06-18 12:29:04 -0700

    creates Home component and some styling

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 import React from 'react';
+import Home from './pages/Home'
 
 class App extends React.Component {
-
-  state = {
-    gm_product_info: []
+  constructor(){
+    super()
+    this.state = {
+      bs_search_detail_info: []
+    }
   }
-  componentDidMount() {
-    fetch('http://localhost:4000/kurt_test')
-    .then(response => response.json())
-    .then(response => this.setState({ gm_product_info: response.data }))
-    .catch(err => console.error(err))
-  } 
 
-
-  renderProduct = <div key={this.state.gm_product_info.category_info_id}></div>
-
-  render() {
-    const { gm_product_info } = this.state;
-
+    componentDidMount () {
+      fetch('http://localhost:4000/home')
+      .then(response => response.json())
+      .then(response => this.setState({ bs_search_detail_info: response.data }))
+      .catch(err => console.error(err))
+    } 
+  
+  
+  render(){
     return (
-      <div className="App">
-        <h1>hey world</h1>
-          {gm_product_info.map(gm_product_info => <div>{gm_product_info.product_name}</div>)}
+      <div>
+        <h1>Got Mary</h1>
+        <Home bs_search_detail_info={this.state.bs_search_detail_info} />
       </div>
     );
   }
---
+++ src/pages/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../App.css'
+
+const Home = (props) => {
+  
+  return (
+   <div>
+      <div className="flex-container wrap">
+        {props.bs_search_detail_info.map(bs_search_detail_info =>
+          <div className="flex-containter wrap">
+            <img className="flex-item" src={bs_search_detail_info.bs_search_detail_image_file}></img>
+            <div>{bs_search_detail_info.bs_search_detail_name}</div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
